7450d5c910f57278c1805757f4e9703e012ba122
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-06-25 10:55:10 +0200

    week-2-Day-1

---
+++ React/Box-Generator/box-generator/src/components/BoxColor.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+const BoxColor = (props) => {
+    const[colorname,setColor] = useState("");
+    const[boxes,setBoxes] = useState([]);
+
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
+
+    const handleChange = (e) => {
+        setColor(e.target.value);
+        
+    };
+
+    const handleAddBox = () =>{
+        if(colorname){
+            setBoxes([...boxes,colorname]);
+            setColor('');
+        }
+    }
+
+    
+
+
+
+
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <label>Color : </label>
+        <input type="text" value={colorname} onChange={handleChange} />
+        <button onClick={handleAddBox}>Add</button>
+        <div style={{display : 'flex',
+         flexWrap : 'wrap',
+         marginTop : "20px"
+    
+    }}>
+        {boxes.map((boxColor, index) => (
+          <div
+            key={index}
+            style={{
+              
+              width: '250px',
+              height: '250px',
+              backgroundColor: boxColor,
+              margin: 'auto',
+            }}
+          />
+        ))}
+        </div>
+        
+    </form>
+  )
+}
+
+export default BoxColor
