84077bd0ea8bf3f6fa7342b05f76c58892acc723
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-06-25 12:01:38 +0200

    week-2-day-1

---
+++ React/Box-generator-bonus/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoxColor from './components/BoxColor';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxColor/>
+    </div>
+  );
+}
+
+export default App;
