2dc0759a8b93e9f2d4d58ab1c7a232ad86baaf76
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-06-26 22:55:17 +0200

    week2-day2

--- React/Pokemon-axios/pokemon/src/App.js
+++ React/Pokemon-axios/pokemon/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import PokemonList from './components/PokemonList';
 
-const App = () => {
+import './App.css';
+import {useState} from "react";
+import axios from 'axios';
+
+function App() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    
+          .then(response => {
+            console.log(response.data.results);
+            setPokemons(response.data.results);
+        }).catch(err=>{
+            console.log(err);
+        });
+  }
+
+  
+
   return (
-    <div>
-      <h1>Pokédex</h1>
-      <PokemonList />
+    <div className="App">
+      <button onClick={fetchPokemons} >Fetch Pokemons</button> <br />
+      <ul>
+      {pokemons.map((pokemon)=>{
+        return( <li>{pokemon.name}</li> )
+      })}
+      </ul>
     </div>
   );
-};
+}
 
 export default App;
-
