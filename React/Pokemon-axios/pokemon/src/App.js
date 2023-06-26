897ba20377de3fc532651f21d86fd241d0ddcdd4
aymenkhamassi <aymenkhammassi2@gmail.com>
2023-06-26 22:47:41 +0200

    week-2-day1

---
+++ React/Pokemon-axios/pokemon/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PokemonList from './components/PokemonList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Pokédex</h1>
+      <PokemonList />
+    </div>
+  );
+};
+
+export default App;
+
